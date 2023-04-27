cc1286773ea02277cfb0090d4f0dc82173079dbd
nguyenminhtri1061997 <nguyenminhtri1061997@gmail.com>
2023-04-27 15:10:57 +0700

    update convert data

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -26,8 +26,63 @@ export class UploadService {
       (t) => !re.test(t) && t !== ' ' && t !== '',
     )
     const resText = arrTextRemovedVN.join('\n')
+    const tmp = resText
+    const questionsRaw = tmp.split(/Question/)
+    const arrTitle = ['Mark the letter ', 'Read  the  following']
+    const objJSON = []
+    let curInstruction
+    questionsRaw.forEach((quest) => {
+      let instruction
+      if (
+        !quest.includes('A.') ||
+        !quest.includes('B.') ||
+        !quest.includes('C.') ||
+        !quest.includes('D.')
+      ) {
+        instruction = quest
+        curInstruction = {
+          instruction,
+          questions: [],
+        }
+        objJSON.push(curInstruction)
+        return
+      }
+      const title = `Question${quest.substring(0, quest.indexOf('A. '))}`
+      const ans = quest.substring(quest.indexOf('A. '), quest.indexOf('D. '))
+      const ansA = ans.substring(ans.indexOf('A. '), ans.indexOf('B. '))
+      const ansB = ans.substring(ans.indexOf('B. '), ans.indexOf('C. '))
+      const ansC = ans.substring(ans.indexOf('C. '))
+
+      const ansDRaw = quest.substring(quest.indexOf('D. '))
+      const ansD = ansDRaw.substring(
+        ansDRaw.indexOf('D. '),
+        ansDRaw.indexOf('\n'),
+      )
+
+      curInstruction.questions.push({
+        title,
+        reusults: {
+          A: ansA.replace('\n', '').trim(),
+          B: ansB.replace('\n', '').trim(),
+          C: ansC.replace('\n', '').trim(),
+          D: ansD.replace('\n', '').trim(),
+        },
+      })
+      arrTitle.forEach((t: string) => {
+        if (arrTitle.includes(t)) {
+          instruction = ansDRaw.substring(ansDRaw.indexOf('\n'))
+        }
+      })
+      if (instruction && instruction !== '\n') {
+        curInstruction = {
+          instruction,
+          questions: [],
+        }
+        objJSON.push(curInstruction)
+      }
+    })
     const pathOutFile = path.join(process.cwd(), 'out.txt')
-    fs.writeFileSync(pathOutFile, resText)
+    fs.writeFileSync(pathOutFile, JSON.stringify(objJSON, null, 2))
     return null
   }
 }
